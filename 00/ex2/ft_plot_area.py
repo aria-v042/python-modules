3b4782452e4c59f1784acd3171df56dafd53be2e
aria <redcore_one@proton.me>
2026-09-22 18:07:59 +0100

    style (00): ex2, ex3: remove type hint from variable initialized at declaration

--- 00/ex2/ft_plot_area.py
+++ 00/ex2/ft_plot_area.py
@@ -3,6 +3,6 @@ def ft_plot_area():
     length = int(input("Enter length: "))
     width = int(input("Enter width: "))
 
-    area: int = length * width
+    area = length * width
 
     print("Plot area:", area)
--- 00/ex3/ft_harvest_total.py
+++ 00/ex3/ft_harvest_total.py
@@ -4,6 +4,6 @@ def ft_harvest_total():
     day2 = int(input("Day 2 harvest: "))
     day3 = int(input("Day 3 harvest: "))
 
-    total: int = day1 + day2 + day3
+    total = day1 + day2 + day3
 
     print("Total harvest:", total)
