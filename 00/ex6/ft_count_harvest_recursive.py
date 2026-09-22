e35f114f489da10173e4f7a359c6f80b7ece01f2
aria <redcore_one@proton.me>
2026-09-22 18:01:01 +0100

    style (00): ex6: rename nested function

--- 00/ex6/ft_count_harvest_recursive.py
+++ 00/ex6/ft_count_harvest_recursive.py
@@ -3,13 +3,13 @@
 #     harvest = int(input("Days until harvest: "))
 #
 #     day = 1
-#     def count_days(day):
+#     def display_day_messages(day):
 #         print("Day", day)
 #         if day == harvest:
 #             print("Harvest time!")
 #         else:
 #             count_days(day + 1)
-#     count_days(day)
+#     display_day_messages(day)
 
 def ft_count_harvest_recursive(day=1, harvest=0):
 
