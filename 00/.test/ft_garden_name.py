f6a38fb9f12f9f529f8ef182d6837d5a076bec7a
aria <redcore_one@proton.me>
2026-09-22 18:21:14 +0100

    test (00): remove exercise file copies

--- 00/.test/ft_garden_name.py
+++
@@ -1,6 +0,0 @@
-def ft_garden_name():
-
-    garden_name: str = input("Enter garden name: ")
-
-    print("Garden: " + garden_name)
-    print("Status: Growing well!")
--- 00/.test/ft_water_reminder.py
+++
@@ -1,5 +0,0 @@
-def ft_water_reminder():
-
-    days = int(input("Days since the last watering: "))
-
-    print("Water the plants!" if days > 2 else "Plants are fine")
