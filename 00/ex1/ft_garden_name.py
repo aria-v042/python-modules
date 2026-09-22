32b9157c86e800816c13bced261b1f383af49d5d
aria <redcore_one@proton.me>
2026-09-22 18:16:05 +0100

    style (00): ex1: remove type hint from variable initialized at declaration

--- 00/ex1/ft_garden_name.py
+++ 00/ex1/ft_garden_name.py
@@ -1,6 +1,6 @@
 def ft_garden_name():
 
-    garden_name: str = input("Enter garden name: ")
+    garden_name = input("Enter garden name: ")
 
     print("Garden: " + garden_name)
     print("Status: Growing well!")
