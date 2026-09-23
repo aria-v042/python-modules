17fabf591d0f34fff199c417df26a000ca8de058
aria <redcore_one@proton.me>
2026-09-23 22:12:27 +0100

    style(01): fix style of ex0

--- 01/ex0/ft_garden_intro.py
+++ 01/ex0/ft_garden_intro.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python
 
 
-def ft_garden_intro(name: str, height: float, age: int) -> None:
-
+def display_plant_info(name: str, height: float, age: int) -> None:
 
     print("Plant:", name.capitalize())
     print("Height: ", height, "cm", sep="")
     print("Age:", age, "days")
 
 
-if __name__ == "__main__":
-
+def ft_garden_intro() -> None:
 
     print("=== Welcome to My Garden ===")
 
-    ## User input ##
+    # # User input
     # name = input("Plant name: ")
     # height = int(input("Plant height: "))
     # age = int(input("Plant age: "))
     # print()
 
-    ## Hard coded ##
+    # Hard coded
     name = "Rose"
     height = 25
     age = 30
 
-    ft_garden_intro(name, height, age)
+    display_plant_info(name, height, age)
 
     print()
     print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+
+    ft_garden_intro()
