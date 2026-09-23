c7d323ce0590bf0e056a6778a6298f6d9fadb666
aria <redcore_one@proton.me>
2026-09-23 23:12:40 +0100

    style(01): fix style on ex1

--- 01/ex1/ft_garden_data.py
+++ 01/ex1/ft_garden_data.py
@@ -3,28 +3,32 @@
 
 class Plant:
 
-
     def __init__(self, name: str, height: float, age: int) -> None:
         self.name = name
         self.height = height
         self.age = age
 
     def show(self) -> None:
-        print(f"{self.name.capitalize()}: {self.height}cm, {self.age} days old")
+        print(f"{self.name.capitalize()}: {self.height}cm, "
+              f"{self.age} days old")
+
+
+def ft_garden_data() -> None:
+
+    # Create Plant instances
+    rose = Plant("rose", 25, 30)
+    sunflower = Plant("sunflower", 80, 45)
+    cactus = Plant("cactus", 15, 120)
+
+    # Welcome message
+    print("=== Garden Plant Registry ===")
+
+    # Display each plant's information
+    rose.show()
+    sunflower.show()
+    cactus.show()
 
 
 if __name__ == "__main__":
 
-
-    ## Create Plant instances ##
-    rose = Plant("rose", 25, 30) 
-    sunflower = Plant("sunflower", 80, 45) 
-    cactus = Plant("cactus", 15, 120) 
-
-    ## Welcome message ##
-    print("=== Garden Plant Registry ===")
-
-    ## Display each plant's information ##
-    rose.show()
-    sunflower.show()
-    cactus.show()
+    ft_garden_data()
