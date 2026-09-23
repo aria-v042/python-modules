3f398b1ba5d26561aaedc3f8d53951dca4c44c73
aria <redcore_one@proton.me>
2026-09-24 00:44:30 +0100

    fix(01): allow instantiating plant objects without setting attributes

--- 01/ex2/ft_plant_growth.py
+++ 01/ex2/ft_plant_growth.py
@@ -3,20 +3,23 @@
 
 class Plant:
 
-    def __init__(self, name: str, height: float, plant_age: int) -> None:
+    def __init__(self,
+                 name: str = "",
+                 height: float = 0,
+                 age_days: int = 0) -> None:
         self.name = name
         self.height = height
-        self.plant_age = plant_age
+        self.age_days = age_days
 
     def show(self) -> None:
         print(f"{self.name.capitalize()}: {self.height:.1f}cm, "
-              f"{self.plant_age} days old")
+              f"{self.age_days} days old")
 
     def grow(self, growth: float) -> None:
         self.height += growth
 
     def age(self, days: int) -> None:
-        self.plant_age += days
+        self.age_days += days
 
 
 def simulate_growth(plant: Plant, daily_growth: float,
@@ -38,7 +41,7 @@ def ft_plant_growth() -> None:
     # # User input values
     # name = input("Plant name: ")
     # height = float(input("Height (cm): "))
-    # plant_age = int(input("Age (days): "))
+    # age_days = int(input("Age (days): "))
     # print()
     # daily_growth = float(input("Plant's daily growth (cm): "))
     # num_of_days = int(input("Number of days: "))
@@ -47,12 +50,15 @@ def ft_plant_growth() -> None:
     # Hard coded values
     name: str = "rose"
     height: float = 25.0
-    plant_age: int = 30
+    age_days: int = 30
     daily_growth = 0.8
     num_of_days = 7
 
-    # Create Plant instance
-    plant = Plant(name, height, plant_age)
+    # Create Plant instance and set its attributes
+    plant = Plant()
+    plant.name = name
+    plant.height = height
+    plant.age_days = age_days
 
     # Simulate/display growth
     simulate_growth(plant, daily_growth, num_of_days)
