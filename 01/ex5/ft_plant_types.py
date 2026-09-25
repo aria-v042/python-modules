3900585f591f249675de17098d96fff3593527ee
aria <redcore_one@proton.me>
2026-09-25 19:02:21 +0100

    refactor(01): ex5: reduce repeating code by creating show_name_inline() method

--- 01/ex5/ft_plant_types.py
+++ 01/ex5/ft_plant_types.py
@@ -40,54 +40,42 @@ class Plant:
     # Setters
 
     def set_name(self, name: str) -> None:
-        if self.get_name() != "":
-            print(f"{self.get_name()}: ", end="")
-        else:
-            print("{unnamed}: ", end="")
+        self.show_name_inline()
         self._name = name.capitalize()
         print(f"Name updated: {self.get_name()}")
 
     def set_height(self, height: float) -> None:
         if height < 0:
-            if self.get_name() != "":
-                print(f"{self.get_name()}: ", end="")
-            else:
-                print("{unnamed}: ", end="")
+            self.show_name_inline()
             print("Error, height can't be negative")
             print("Height update rejected")
         else:
             self._height = height
-            if self.get_name() != "":
-                print(f"{self.get_name()}: ", end="")
-            else:
-                print("{unnamed}: ", end="")
+            self.show_name_inline()
             print(f"Height updated: {self.get_height()}cm")
 
     def set_age(self, age: int) -> None:
         if age < 0:
-            if self.get_name() != "":
-                print(f"{self.get_name()}: ", end="")
-            else:
-                print("{unnamed}: ", end="")
+            self.show_name_inline()
             print("Error, age can't be negative")
             print("Age update rejected")
         else:
             self._age = age
-            if self.get_name() != "":
-                print(f"{self.get_name()}: ", end="")
-            else:
-                print("{unnamed}: ", end="")
+            self.show_name_inline()
             print(f"Age updated: {self.get_age()} days")
 
     # Plant methods
 
     def show(self) -> None:
-        if self.get_name() != "":
-            print(f"{self.get_name()}: ", end="")
-        else:
-            print("{unnamed}: ", end="")
+        self.show_name_inline()
         print(f"{self.get_height():.1f}cm, "
               f"{self.get_age()} days old")
+
+    def show_name_inline(self) -> None:
+        if self.get_name() == "":
+            print("{unnamed}: ", end="")
+        else:
+            print(f"{self.get_name()}: ", end="")
 
     def grow(self, growth: float) -> None:
         self.set_height(self.get_height() + growth)
@@ -118,6 +106,8 @@ class Flower(Plant):
 
     def set_color(self, color: str) -> None:
         self._color = color
+        self.show_name_inline()
+        print(f"Color updated: {self.get_color()}")
 
     # Flower methods
 
@@ -142,8 +132,7 @@ class Tree(Plant):
         super().__init__(name, height, age)
         if trunk_diameter < 0:
             self._trunk_diameter = 0
-            print(f"{super().get_name()}: "
-                  "Error, trunk diameter can't be negative")
+            print("Error, trunk diameter can't be negative")
         else:
             self._trunk_diameter = trunk_diameter
 
@@ -156,10 +145,13 @@ class Tree(Plant):
 
     def set_trunk_diameter(self, trunk_diameter: float) -> None:
         if trunk_diameter < 0:
-            print(f"{super().get_name()}: "
-                  "Error, trunk diameter can't be negative")
+            self.show_name_inline()
+            print("Error, trunk diameter can't be negative")
+            print("Trunk diameter update rejected")
         else:
             self._trunk_diameter = trunk_diameter
+            self.show_name_inline()
+            print(f"Trunk diameter updated: {self.get_trunk_diameter():.1f}cm")
 
     # Tree methods
 
@@ -187,8 +179,7 @@ class Vegetable(Plant):
         self._harvest_season = harvest_season.capitalize()
         if nutritional_value < 0:
             self._nutritional_value = 0
-            print(f"{super().get_name()}: "
-                  "Error, nutritional value can't be negative")
+            print("Error, nutritional value can't be negative")
         else:
             self._nutritional_value = nutritional_value
 
@@ -204,13 +195,17 @@ class Vegetable(Plant):
 
     def set_harvest_season(self, harvest_season: str) -> None:
         self._harvest_season = harvest_season.capitalize()
+        self.show_name_inline()
+        print(f"Harvest season updated: {self.get_harvest_season()}")
 
     def set_nutritional_value(self, nutritional_value: int) -> None:
         if nutritional_value < 0:
-            print(f"{super().get_name()}: "
-                  "Error, nutritional value can't be negative")
+            self.show_name_inline()
+            print("Error, nutritional value can't be negative")
         else:
             self._nutritional_value = nutritional_value
+            self.show_name_inline()
+            print(f"Nutritional value updated: {self.get_nutritional_value()}")
 
     # Vegetable methods
 
